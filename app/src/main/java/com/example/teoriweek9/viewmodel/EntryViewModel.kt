04fbf6670ed46ac64aa378fffd46c82d58327341
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-27 16:34:02 +0700

    menambahkan class HomeViewModel

---
+++ app/src/main/java/com/example/teoriweek9/viewmodel/EntryViewModel.kt
@@ -0,0 +1,2 @@
+package com.example.teoriweek9.viewmodel
+
---
+++ app/src/main/java/com/example/teoriweek9/viewmodel/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.teoriweek9.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.teoriweek9.Repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+}
