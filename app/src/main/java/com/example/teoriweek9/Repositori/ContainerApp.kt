eadef2cd562a7a4b286674b65f5b1013f8980080
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-27 16:22:43 +0700

    menambahkan container data app

--- app/src/main/java/com/example/teoriweek9/Repositori/ContainerApp.kt
+++ app/src/main/java/com/example/teoriweek9/Repositori/ContainerApp.kt
@@ -1,5 +1,12 @@
 package com.example.teoriweek9.Repositori
 
+import android.content.Context
+
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
+
+class ContainerDataAPp(private val context: Context):
+        ContainerApp{
+
+        }
