0575009f04ee94c67440098c9bc6a94ca4d613ca
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-27 16:24:59 +0700

    menambahkan offlineRepositoriSiswa

--- app/src/main/java/com/example/teoriweek9/Repositori/ContainerApp.kt
+++ app/src/main/java/com/example/teoriweek9/Repositori/ContainerApp.kt
@@ -1,6 +1,7 @@
 package com.example.teoriweek9.Repositori
 
 import android.content.Context
+import com.example.teoriweek9.room.DatabaseSiswa
 
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
@@ -9,6 +10,7 @@ interface ContainerApp {
 class ContainerDataAPp(private val context: Context):
         ContainerApp{
             override val repositoriSiswa: RepositoriSiswa by lazy {
-
+                OfflineRepositoriSiswa(
+                siswaDao = DatabaseSiswa.getDatabase(context).siswaDao())
             }
         }
