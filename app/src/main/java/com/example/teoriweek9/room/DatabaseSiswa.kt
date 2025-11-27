5ce62fa9e1245f96a75b992af8c8fdf32270f2d0
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-27 16:06:38 +0700

    menambahkan function untuk abstract class

--- app/src/main/java/com/example/teoriweek9/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/teoriweek9/room/DatabaseSiswa.kt
@@ -1,6 +1,10 @@
 package com.example.teoriweek9.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
+}
