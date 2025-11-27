5f495ae0b2495a73df29cd46c88b2b653ffa2e44
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-27 16:19:47 +0700

    menambahkan function getAllSiswa

--- app/src/main/java/com/example/teoriweek9/Repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/teoriweek9/Repositori/RepositoriSiswa.kt
@@ -1,6 +1,7 @@
 package com.example.teoriweek9.Repositori
 
 import com.example.teoriweek9.room.Siswa
+import com.example.teoriweek9.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
@@ -10,4 +11,7 @@ interface RepositoriSiswa {
 
 class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
-)
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+
+}
