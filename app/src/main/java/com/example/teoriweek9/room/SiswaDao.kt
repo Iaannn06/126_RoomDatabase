5264ff65119b41cf12415f7a58cbc24514603d58
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-27 15:58:43 +0700

    menambahkan function getAllSiswa

--- app/src/main/java/com/example/teoriweek9/room/SiswaDao.kt
+++ app/src/main/java/com/example/teoriweek9/room/SiswaDao.kt
@@ -1,7 +1,11 @@
 package com.example.teoriweek9.room
 
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 interface SiswaDao {
     @Query(value ="SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+
 }
