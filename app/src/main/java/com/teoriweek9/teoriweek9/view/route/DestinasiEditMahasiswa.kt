bed010b33bd04bd6103a55d997a5c4881afd9bad
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-04 20:16:02 +0700

    perbaikan DestinasiEditMahasiswa.kt

---
+++ app/src/main/java/com/teoriweek9/teoriweek9/view/route/DestinasiEditMahasiswa.kt
@@ -0,0 +1,10 @@
+package com.teoriweek9.teoriweek9.view.route
+
+import com.teoriweek9.teoriweek9.R
+
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
