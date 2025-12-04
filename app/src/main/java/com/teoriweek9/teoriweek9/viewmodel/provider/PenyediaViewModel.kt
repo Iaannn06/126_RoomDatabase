0e0f963e61653758181849558671144e72d5a029
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-04 16:06:42 +0700

    perbaikan pada PenyediaViewModel.kt

--- app/src/main/java/com/teoriweek9/teoriweek9/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/teoriweek9/teoriweek9/viewmodel/provider/PenyediaViewModel.kt
@@ -7,46 +7,35 @@ import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.tu.repositori.AplikasiSiswa
 import com.example.myroomsatu.viewmodel.EntryViewModel
 import com.example.teoriweek9.viewmodel.DetailViewModel
 import com.teoriweek9.teoriweek9.Repositori.AplikasiSiswa
 import com.teoriweek9.teoriweek9.viewmodel.HomeViewModel
 
-/**
- * Objek yang menyediakan instance [ViewModelFactory] untuk setiap Layar (Screen) Compose.
- */
 object PenyediaViewModel {
-
-    /**
-     * Objek [ViewModelFactory] untuk membuat instance [HomeViewModel] dan [EntryViewModel]
-     * dan meneruskan dependensi yang diperlukan.
-     */
     val Factory = viewModelFactory {
-        // Inisialisasi HomeViewModel
         initializer {
-            // Mengambil RepositoriSiswa dari container AplikasiSiswa
             HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
 
         initializer {
-            DetailViewModel(savedStateHandle=this.createSavedStateHandle(),
-                repositoriSiswa = aplikasiSiswa().container.repositoriSiswa)
-        }
-
-        // Inisialisasi EntryViewModel
-        initializer {
-            // Mengambil RepositoriSiswa dari container AplikasiSiswa
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        //initializer {
+        //    EditViewModel(this.createSavedStateHandle(),aplikasiSiswa(),container.repositoriSiswa)
+        //}
     }
 }
 
 /**
- * Fungsi ekstensi [CreationExtras] untuk mengambil objek [Application] dan
- * mengembalikan instance dari [AplikasiSiswa].
- */
+
+Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah
+instance dari [AplikasiSiswa].*/
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as Application).let {
-        it as AplikasiSiswa
-    }
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
