131ab5f882a7bc9871d8d76400919f8730f22023
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-04 15:52:35 +0700

    perbaikan pada RepositoriSiswa

--- app/src/main/java/com/teoriweek9/teoriweek9/Repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/teoriweek9/teoriweek9/Repositori/RepositoriSiswa.kt
@@ -1,17 +1,27 @@
-package com.teoriweek9.teoriweek9.Repositori
+package com.teoriweek9.teoriweek9.room
 
-import com.teoriweek9.teoriweek9.room.Siswa
-import com.teoriweek9.teoriweek9.room.SiswaDao
+
+
+import androidx.room.Insert
+
+import androidx.room.OnConflictStrategy
+
+import androidx.room.Query
+
 import kotlinx.coroutines.flow.Flow
 
-interface RepositoriSiswa {
-    fun getAllSiswaStream(): Flow<List<Siswa>>
-    suspend fun insertSiswa(siswa:  Siswa)
-}
 
+interface RepositoriSiswa{
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
+}
 class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
